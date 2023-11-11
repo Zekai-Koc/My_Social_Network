2fecf80c0bf596bf79432067afdd3fa2f2dda8d2
Zekai Koç <zekaikoc@gmail.com>
2023-11-11 16:40:16 +0100

    user profile image added.

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "cc3bc307c10eb853"; }
+/******/ 		__webpack_require__.h = function() { return "6be01c405015731a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/user/profile/update.js
+++ client/pages/user/profile/update.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
-import { Modal } from "antd";
+import { Modal, Avatar } from "antd";
 import Link from "next/link";
 import AuthForm from "../../../components/forms/AuthForm.js";
 import { UserContext } from "../../../context";
 import { useContext } from "react";
 import { useRouter } from "next/router";
+import { LoadingOutlined, CameraOutlined } from "@ant-design/icons";
 
 const ProfileUpdate = () => {
    const [username, setUsername] = useState("");
@@ -17,6 +18,8 @@ const ProfileUpdate = () => {
    const [secret, setSecret] = useState("");
    const [ok, setOk] = useState(false);
    const [loading, setLoading] = useState(false);
+   const [image, setImage] = useState({});
+   const [uploading, setUploading] = useState(false);
 
    const [state, setState] = useContext(UserContext);
    const router = useRouter();
@@ -27,6 +30,7 @@ const ProfileUpdate = () => {
       setAbout(state.user.about);
       setName(state.user.name);
       setEmail(state.user.email);
+      setImage(state.user.image);
       // setSecret(state.user.state);
    }, [state && state.user]);
 
@@ -41,6 +45,7 @@ const ProfileUpdate = () => {
             email,
             password,
             secret,
+            image,
          });
 
          console.log("data", data);
@@ -64,6 +69,27 @@ const ProfileUpdate = () => {
       }
    };
 
+   const handleImage = async (e) => {
+      const file = e.target.files[0];
+      let formData = new FormData();
+      formData.append("image", file);
+      // console.log([...formData]);
+      setUploading(true);
+      try {
+         const { data } = await axios.post("/post/uploadimage", formData);
+         // console.log("Uploaded Image: ", data);
+
+         setImage({
+            url: data.url,
+            public_id: data.public_id,
+         });
+         setUploading(false);
+      } catch (error) {
+         console.log(error);
+         setUploading(false);
+      }
+   };
+
    return (
       <div className="container-fluid">
          <div className="row py-5 bg-secondary text-light bg-default-image">
@@ -74,6 +100,23 @@ const ProfileUpdate = () => {
          {/* {loading ? <h1>Loading</h1> : ""} */}
          <div className="row py-5">
             <div className="col-md-6 offset-md-3">
+               <label className="d-flex justify-content-center h5">
+                  {image && image.url ? (
+                     <Avatar size={30} src={image.url} className="mt-1" />
+                  ) : uploading ? (
+                     <LoadingOutlined className="nt-2" />
+                  ) : (
+                     <CameraOutlined className="nt-2" />
+                  )}
+
+                  <input
+                     onChange={handleImage}
+                     type="file"
+                     accept="images/*"
+                     hidden
+                  />
+               </label>
+
                <AuthForm
                   handleSubmit={handleSubmit}
                   name={name}
